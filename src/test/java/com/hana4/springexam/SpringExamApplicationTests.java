f42ca7fc7308eba475d60772a00c13ca3e66f072
Sung Yeob Jung <dev.sungyeob@gmail.com>
2024-12-08 16:00:06 +0900

    first commmit

---
+++ src/test/java/com/hana4/springexam/SpringExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana4.springexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
